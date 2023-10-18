bac234d3182b203be6ee2adf237162f6a66e2404
Chetan-80097 <chetanarote5@gmail.com>
2023-10-18 19:26:52 +0530

    Complete Que 1,2,4 of Day12Assignment uploaded

---
+++ Day12Assignment/day12assignment_que3/src/com/sunbeam/ManipulateEmployee.java
@@ -0,0 +1,14 @@
+package com.sunbeam;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ManipulateEmployee {
+
+	public static void main(String[] args) {
+
+		List<Employee> list = new LinkedList<Employee>();
+	}
+
+}
